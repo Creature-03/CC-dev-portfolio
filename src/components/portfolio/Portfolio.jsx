46f2399398fd6c83ee4d305b2950aa96e4f8090b
Andrew Meyer <meyer.r.andrew@gmail.com>
2022-05-30 00:14:29 -0700

    created portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './portfolio.css'
+import CCMoviesIMG from '../../assets/CCMovies.PNG'
+
+const data = [
+  {
+    id: 1,
+    image: CCMoviesIMG,
+    title: 'CreatureCodes Movies',
+    github: 'https://github.com/Creature-03/CreatureCodesMovies',
+    demo: 'https://creature-codes-movies.netlify.app'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolioContainer">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article className='portfolioItem' key={id}>
+                <div className="portfolioItemImg">
+                  <img src={image} alt={title} />
+                </div>
+                  <h3>{title}</h3>
+                <div className="porfolioItemCTA">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
